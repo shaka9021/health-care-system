8e807d295ce3bc126c2f7a3ec437826114c8f012
Paula Angel <paulaangto@gmail.com>
2019-03-31 09:33:32 -0500

    agregando imagenes

---
+++ Consultas.sql
@@ -0,0 +1,14 @@
+select * from consulta;
+select * from especialidad;
+select * from paciente;
+select * from medico;
+select * from cita;
+select * from detalle_pago;
+select * from dia_medico;
+select * from hora_medico;
+select * from pago;
+select * from servicio; 
+
+
+
+
